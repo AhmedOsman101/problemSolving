24f56a73fa88ccfe332c6a4780223a5fbab446da
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-03 01:16:47 +0300

    feat: add MajorityElement.php to find the majority element in an array
    - Implemented the `majorityElement` function using `array_count_values` and `array_search`.
    - Added an alternative implementation using a manual counting method.
    - Tested the function with an example array.

---
+++ MajorityElement.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Given an array nums of size n, return the majority element.
+
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+
+ * @param int[] $nums
+ * @return int
+ */
+
+
+function majorityElement($nums): int {
+  $nums = array_count_values($nums);
+  return array_search(max($nums), $nums);
+}
+
+
+/* another solution
+function majorityElement($nums): int {
+  $temp = [];
+
+  foreach ($nums as $num) {
+    isset($temp[$num]) ? $temp[$num]++ : $temp[$num] = 1;
+  }
+
+  $target = max($temp);
+
+  foreach ($temp as $key => $val) {
+    if ($val === $target) return $key;
+  }
+}
+*/
+
+$nums = [3, 3, 4];
+
+echo majorityElement($nums);
