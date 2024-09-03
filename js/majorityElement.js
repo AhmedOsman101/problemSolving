9c9841861e44e7a58145c2b97d48477c21c55544
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-03 22:28:07 +0300

    update majorityElement.js

--- js/majorityElement.js
+++ js/majorityElement.js
@@ -2,27 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
-    let result = {}
-    // check if number in object
-    for(let i =0; i < nums.length;i++) {
-        let cur = nums[i]
-        result[cur] = (result[cur] || 0) + 1
+var majorityElement = function (nums) {
+  let result = {};
+  // check if number in object
+  for (let i = 0; i < nums.length; i++) {
+    let cur = nums[i];
+    result[cur] = (result[cur] || 0) + 1;
+  }
+  console.log(result);
+  // get the max key of object
+  let maxkey = 0;
+  let maxVal = -Infinity;
+  for (let [key, value] of Object.entries(result)) {
+    if (value > maxVal) {
+      maxVal = value;
+      maxkey = +key;
     }
-    console.log(result);
-    // get the max key of object
-    let maxkey = 0;
-    let maxVal = -Infinity; 
-    for(let [key,value] of Object.entries(result)){
-        if(value > maxVal){
-            maxVal = value
-            maxkey = +key
-        }
-    }
+  }
 
-    return maxkey
+  return maxkey;
 };
 
-
-
-majorityElement( [3,3,2])
+console.log(majorityElement([3, 3, 2]));
