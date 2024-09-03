315feb3c56d91e6165522f79d32c281ab2d87d3f
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2024-09-03 18:36:23 +0300

    majorityElement in js

---
+++ js/majorityElement.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let result = {}
+    // check if number in object
+    for(let i =0; i < nums.length;i++) {
+        let cur = nums[i]
+        result[cur] = (result[cur] || 0) + 1
+    }
+    console.log(result);
+    // get the max key of object
+    let maxkey = 0;
+    let maxVal = -Infinity; 
+    for(let [key,value] of Object.entries(result)){
+        if(value > maxVal){
+            maxVal = value
+            maxkey = +key
+        }
+    }
+
+    return maxkey
+};
+
+
+
+majorityElement( [3,3,2])
