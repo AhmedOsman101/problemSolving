ca4aae6a687ecf20648b620fedab45f4bba3bff2
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-03 22:26:36 +0300

    update mergeSortedArray.js

--- js/mergeSortedArray.js
+++ js/mergeSortedArray.js
@@ -5,13 +5,17 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    var arr1 = nums1.slice(0,m)
-    var arr2 = nums2.slice(0,n)
-    arr1 = [...arr1,...arr2].sort((a,b)=>a-b) // callback is very common here to sort numbers in js
-    for(let i=0;i<nums1.length;i++){
-        nums1[i] = arr1[i]
-    }
+var merge = function (nums1, m, nums2, n) {
+  var arr1 = nums1.slice(0, m);
+  var arr2 = nums2.slice(0, n);
+  arr1 = [...arr1, ...arr2].sort((a, b) => a - b); // callback is very common here to sort numbers in js
+  for (let i = 0; i < nums1.length; i++) {
+    nums1[i] = arr1[i];
+  }
 };
 
-merge([1,2,3,0,0,0],3,[2,5,6],3)
+const nums1 = [1, 2, 3, 0, 0, 0];
+
+merge(nums1, 3, [2, 5, 6], 3);
+
+// console.log(nums1);
