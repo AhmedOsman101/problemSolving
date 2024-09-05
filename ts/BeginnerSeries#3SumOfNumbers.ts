ac47eb35bf74624a8d662b004b74f28f2861424e
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-06 00:12:17 +0300

    feat: add function to sum integers between two values
    - Implemented `getSum` function to calculate the sum of all integers between and including two given numbers.
    - The function handles positive and negative integers, and returns the single number if both inputs are equal.

---
+++ ts/BeginnerSeries#3SumOfNumbers.ts
@@ -0,0 +1,38 @@
+/**
+ * Given two integers `a` and `b`, which can be positive or negative, find the sum
+ * of all the integers between and including them and return it. If the two
+ * numbers are equal return `a` or `b`.
+
+ * Note: `a` and `b` are not ordered!
+
+ * @param a
+ * @param b
+ * @returns
+ */
+export function getSum(a: number, b: number): number {
+  if (a === b) return a;
+
+  let min: number, max: number;
+
+  if (a < b) {
+    min = a;
+    max = b;
+  } else {
+    min = b;
+    max = a;
+  }
+
+  let sum: number = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.info(getSum(1, 0));
+console.info(getSum(1, 2));
+console.info(getSum(0, 1));
+console.info(getSum(1, 1));
+console.info(getSum(-1, 0));
+console.info(getSum(-1, 2));
