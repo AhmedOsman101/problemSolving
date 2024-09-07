ab59bd3df3a284511cf3465873027e03d967a0b1
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-07 16:08:01 +0300

    feat: add function to calculate the number of blank pages needed for paperwork
    - Implemented `paperwork` function to calculate the total number of blank pages needed based on the number of classmates and pages.
    - Added checks to return `0` if either number of classmates or pages is negative.

---
+++ ts/BeginnerSeries#1SchoolPaperwork.ts
@@ -0,0 +1,15 @@
+/**
+ * https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/typescript
+ *
+ * Your classmates asked you to copy some paperwork for them. You know that
+ * there are `n` classmates and the paperwork has `m` pages.
+
+ * Your task is to calculate how many blank pages do you need. If `n < 0` or `m < 0` return `0`.
+ */
+export function paperwork(n: number, m: number): number {
+  if (n < 0 || m < 0) return 0;
+  return n * m;
+}
+
+console.info(paperwork(5, 5)); // 5 * 5 = 25
+console.info(paperwork(5, -5)); // m < 0 -> 0
