864d11eb9b55cbc8781c8692b02dad6c6193f309
AhmedOsman101 <trollingosman101@outlook.com>
2024-09-08 16:54:52 +0300

    refactor: Renamed the method name to avoid duplication

--- js/takeTenMinToWalk.js
+++ js/takeTenMinToWalk.js
@@ -1,7 +1,7 @@
 
 // https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
 
-function isValidWalk(walk) {
+function isValidWalk2(walk) {
     //insert brilliant code here
     if(walk.length != 10 ) return false
     const count = {
@@ -10,10 +10,10 @@ function isValidWalk(walk) {
       n:0,
       s:0
     }
-    
+
     walk.map(item=>{
       return count[item]++
     })
-    
+
     return count.s === count.n && count.w === count.e
   }
