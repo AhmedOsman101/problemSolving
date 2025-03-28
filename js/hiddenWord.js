e45717b2a5a5e0c0aea97caf6758fa38da0cf25a
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-28 14:27:31 +0200

    Updated Files 2025-03-28@02:27PM

---
+++ js/hiddenWord.js
@@ -0,0 +1,31 @@
+/**
+ * returns the hidden word from the given number
+ *
+ * @param {number} num - The magic number
+ * @returns {string} The hidden word
+ */
+function hiddenWord(num) {
+  const chars = "oblietadnm";
+
+  return num
+    .toString()
+    .split("")
+    .map(n => chars[n])
+    .join("");
+}
+
+function hiddenWord2(num) {
+  const chars = "oblietadnm";
+
+  const nums = num.toString().split("");
+
+  let str = "";
+
+  for (const item of nums) {
+    str += chars[item];
+  }
+
+  return str;
+}
+console.log(hiddenWord(113678973)); // bbiadnmdi
+console.log(hiddenWord2(113678973)); // bbiadnmdi
